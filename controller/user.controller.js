054fcc2e4bda41fb666a1313c280fca12586a1d8
sajithapislk <sajith@apis.lk>
2023-07-15 13:19:08 +0530

    add login and release token

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -15,3 +15,37 @@ exports.register = async (req, res, next) => {
     throw error;
   }
 };
+
+exports.login = async (req, res, next) => {
+  try {
+    console.log("---req body---", req.body);
+    const { email, password } = req.body;
+
+    const user = await UserService.checkUser(email);
+    console.log("---user---", user);
+
+    if(!user){
+      throw new Error('User dont exist');
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if(isMatch === false){
+      throw new Error('Password is invalid');
+    }
+
+    let tokenData = {
+      _id:user._id,
+      email:user.email
+    };
+
+    const token = await UserService.generateToken(tokenData,"secretKey",'1h');
+
+    res.status(200).json({
+      status: true,
+      token: token
+    });
+
+  } catch (error) {
+    throw error;
+  }
+};
--- services/user.services.js
+++ services/user.services.js
@@ -1,14 +1,25 @@
-const UserModel = require('../model/user.model');
+const UserModel = require("../model/user.model");
+const jwt = require('jsonwebtoken');
 
-class UserService{
-    static async registerUser(email,password){
-        try {
-            const createUser = new UserModel({email,password});
-            return await createUser.save();
-        } catch (error) {
-            throw error;
-        }
+class UserService {
+  static async registerUser(email, password) {
+    try {
+      const createUser = new UserModel({ email, password });
+      return await createUser.save();
+    } catch (error) {
+      throw error;
     }
+  }
+  static async checkUser(email) {
+    try {
+      return await UserModel.findOne({ email });
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async generateToken(tokenData,secretKey,jwt_expire){
+    return jwt.sign(tokenData,secretKey,{expiresIn:jwt_expire})
+  }
 }
 
 module.exports = UserService;
